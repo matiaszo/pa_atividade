f5aab13492821d56cbed5b4fdb60eb854e58b283
matiaszo <tiaszuniga@gmail.com>
2024-09-25 17:14:51 -0300

    fazendo overflow

---
+++ src/app/layout.tsx
@@ -0,0 +1,40 @@
+import localFont from "next/font/local";
+import { Roboto } from "next/font/google" // titles 
+import { Open_Sans } from "next/font/google" // secondary texts
+import "./globals.css";
+import { Header } from "@/components/header";
+import { Body } from "@/components/body";
+import { Overflow } from "@/components/overflow";
+
+const roboto = Roboto({
+  weight: ["100", "400", "900"],
+  style:"normal",
+  variable: "--roboto",
+  subsets: ["latin"]
+})
+
+const open_sans = Open_Sans({
+  weight: ["300", "600", "800"],
+  style:"normal",
+  variable: "--sans",
+  subsets: ["latin"]
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+      className={`${roboto.variable}`}
+      >
+        <Header/>
+        {children}
+        <Body/>
+        <Overflow/>
+      </body>
+    </html>
+  );
+}
---
+++ src/components/header.jsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import Image from "next/image";
+import scienceImg from "@/img/science.png"
+
+export const Header = () => {
+    return(
+        <>
+        <div className="font-roboto flex place-content-between p-2 mt-8">
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-7">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 3.104v5.714a2.25 2.25 0 0 1-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 0 1 4.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0 1 12 15a9.065 9.065 0 0 0-6.23-.693L5 14.5m14.8.8 1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0 1 12 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5" />
+                </svg>
+            </div>
+
+            <div className="bg-grey rounded-md flex place-content-around items-center w-200 h-15 p-1">
+                <div className="bg-white flex items-center justify-center rounded-md w-24">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                    <p>Search</p>
+                </div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.0" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z" />
+                </svg>
+            </div>
+
+            <div className="bg-blue rounded-full p-1">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                </svg>
+
+            </div>
+
+        </div>
+        
+        </>
+
+    )
+}
